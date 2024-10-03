974f70dd40b25fd0a98dacfd3cafe9cd08544dfe
pinocchio-life-like <icbr19fl@gmail.com>
2024-10-03 12:56:11 +0300

    demo

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { useAuth, AuthProvider } from '../../app/context/AuthContext'; // Ensure you use AuthProvider
 import { useRouter } from 'next/router';
 import LoginForm from '../../app/components/LoginForm'; // Assume you have this component
+import '../../app/globals.css';
 
 const LoginPage: React.FC = () => {
   const { isAuthenticated } = useAuth();
